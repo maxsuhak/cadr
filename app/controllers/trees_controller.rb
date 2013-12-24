ac1260e4096e425bcc974cb8a1e6313947bbc54b
maxsuhak <maxsuhak@gmail.com>
2013-12-24 11:37:13 +0200

    added icon, fix header, erb to slim, added routes, added users controller,

--- app/controllers/trees_controller.rb
+++ app/controllers/trees_controller.rb
@@ -1,6 +1,3 @@
 class TreesController < ApplicationController
   inherit_resources
-  def index
-    @users = User.all
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Cadr::Application.routes.draw do
+  
   devise_for :users
-  resources :languages
+  
+  resources :users, only: [:index, :show]
+  resources :languages, only: [:index]
   resources :trees, only: [:index]
+  
   root to: "trees#index"
+  
+  namespace :admin do
+    resources :users
+    resources :trees
+  end
 end
