3f4e77d47cdb1703fcc146d14ac44dda1f3e594f
maxsuhak <maxsuhak@gmail.com>
2014-01-09 15:40:11 +0200

    fix menu, added dashboards

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,20 @@
 Cadr::Application.routes.draw do
   
+  get "dashboards/index"
+
   devise_for :admins
   devise_for :users
   
-  root to: "trees#index"
+  root to: "dashboards#index"
   
   resources :users, only: [:index, :show, :edit, :update]
   resources :languages, only: [:index]
   resources :trees, only: [:index]
+  resources :dashboards
   
   namespace :admin do
-    root to: "trees#index"
+    root to: "dashboards#index"
+    resources :dashboards
     resources :users
     resources :trees
   end
