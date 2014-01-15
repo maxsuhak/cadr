5f85c9360a7771c9dfaceb1cd39197920ae41a2f
maxsuhak <maxsuhak@gmail.com>
2014-01-15 11:34:16 +0200

    added admin::dashboards

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -1,3 +1,6 @@
 class Admin::DashboardsController < Admin::BaseController
   inherit_resources
+  def show
+    redirect_to admin_dashboards_path
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,6 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 
-User.create(email: "maxsuhak@gmail.com", password: "1q2w3e4r", first_name: "max", last_name: "suhak", birthday: "1992-04-24", lenguage: "ru")
-
 Admin.create(email: "maxsuhak@gmail.com", password: "1q2w3e4r")
 
 Tree.create([{title: "Management Team", img: "http://www.rrcgroup.com/Portals/0/images/executive_team.jpg"}, 
