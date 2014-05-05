127929f585bb4ba1dfa83e73c99706a88164e434
Semenets Bohdan <b.semenets@svitla.com>
2014-05-05 14:44:46 +0300

    fix views event

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,4 +1,2 @@
 class CalendarsController < ApplicationController
-  def show
-  end
 end
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,17 +1,17 @@
 class EventsController < ApplicationController
 
+  before_filter :find_event, only: [:show, :edit, :update, :destroy]
+
   def index
     @events = Event.scoped
     @events = Event.between(params['start'], params['end']) if (params['start'] && params['end'])
     respond_to do |format|
-      format.html 
+      format.html { redirect_to calendar_path }
       format.json { render json: @events }
     end
   end
   
   def show
-    @event = Event.find(params[:id])
-
     respond_to do |format|
       format.html 
       format.json { render json: @event }
@@ -20,52 +20,48 @@ class EventsController < ApplicationController
   
   def new
     @event = Event.new
-
     respond_to do |format|
       format.html 
       format.json { render json: @event }
     end
   end
   
-  def edit
-    @event = Event.find(params[:id])
-  end
-  
   def create
     @event = Event.new(params[:event])
-
     respond_to do |format|
       if @event.save
-        format.html { redirect_to @event, notice: 'Event was successfully created.' }
+        format.html { redirect_to calendar_path, notice: 'Event was successfully created.' }
         format.json { render json: @event, status: :created, location: @event }
       else
-        format.html { render action: "new" }
+        format.html { render action: 'new' }
         format.json { render json: @event.errors, status: :unprocessable_entity }
       end
     end
   end
   
   def update
-    @event = Event.find(params[:id])
-
     respond_to do |format|
       if @event.update_attributes(params[:event])
-        format.html { redirect_to @event, notice: 'Event was successfully updated.' }
+        format.html { redirect_to calendar_path, notice: 'Event was successfully updated.' }
         format.json { head :no_content }
       else
-        format.html { render action: "edit" }
+        format.html { render action: 'edit' }
         format.json { render json: @event.errors, status: :unprocessable_entity }
       end
     end
   end
   
   def destroy
-    @event = Event.find(params[:id])
     @event.destroy
-
     respond_to do |format|
       format.html { redirect_to events_url }
       format.json { head :no_content }
     end
   end
+
+  private
+
+    def find_event
+      @event = Event.find(params[:id])
+    end
 end
