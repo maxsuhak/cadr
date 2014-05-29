8b9e6db7f13184b1e713e30a6d0d8665da093e4e
Semenets Bohdan <b.semenets@svitla.com>
2014-05-29 21:43:00 +0300

    fix usermailer

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -6,14 +6,11 @@ class Admin::UsersController < Admin::BaseController
   end
 
   def show
+    UserMailer.after_update(User.find(params[:id])).deliver
     redirect_to admin_users_path
   end
 
   def unsorted
     @users = User.unsorted.paginate(page: params[:page], per_page: 5).order('created_at DESC')
-  end
-
-  def show
-    redirect_to admin_users_path
   end
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,4 +7,10 @@ class UserMailer < ActionMailer::Base
   def device_registration_success_email(user)
     user.send_confirmation_instructions
   end
+
+  def after_update(user)
+  	@user = user
+  	@url = root_url
+  	mail(to: @user.email, subject: "обновление данных юзера")
+  end
 end
