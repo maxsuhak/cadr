73716b6c6750618512f575c40baafb4c770858dd
Semenets Bohdan <b.semenets@svitla.com>
2014-05-29 20:10:18 +0300

    added confirmation

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,10 +2,14 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
     @user = User.find_for_google_oauth2(request.env["omniauth.auth"], current_user)
     flash[:notice] = I18n.t "devise.omniauth_callbacks.success", kind: "Google"
+
     if @user == nil
       redirect_to root_url
-    else
+    elsif @user.confirmed?
       sign_in_and_redirect @user, event: :authentication
+    else
+      UserMailer.device_registration_success_email(@user).deliver
+      redirect_to root_url
     end
   end
 end
